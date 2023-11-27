5e69482e9b40c425399215f4e1766393175efe1a
Maksymilian Grupiński <maks.grupinski@solve.md>
2023-11-27 23:36:29 +0100

    Added git signs

--- lua/Maks/manager.lua
+++ lua/Maks/manager.lua
@@ -1,78 +1,81 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "nvim-lua/plenary.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.4",
-  },
-  {
-    "catppuccin/nvim",
-    as = "catppuccin",
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  },
-  {
-    "ThePrimeagen/harpoon",
-  },
-  {
-    "mbbill/undotree",
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
-  {
-    "hrsh7th/nvim-cmp",
-  },
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "hrsh7th/cmp-buffer",
-  },
-  {
-    "hrsh7th/cmp-path",
-  },
-  {
-    "hrsh7th/cmp-nvim-lua",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-  },
-  {
-    "VonHeikemen/lsp-zero.nvim",
-  },
-  {
-    "williamboman/mason.nvim",
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  {
-    "nvimtools/none-ls.nvim",
-  },
-  {
-    "georgewfraser/java-language-server",
-  },
-  {
-    "m4xshen/autoclose.nvim",
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-  },
+    {
+        "nvim-lua/plenary.nvim",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+    },
+    {
+        "catppuccin/nvim",
+        as = "catppuccin",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    },
+    {
+        "ThePrimeagen/harpoon",
+    },
+    {
+        "mbbill/undotree",
+    },
+    {
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "hrsh7th/nvim-cmp",
+    },
+    {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    {
+        "hrsh7th/cmp-buffer",
+    },
+    {
+        "hrsh7th/cmp-path",
+    },
+    {
+        "hrsh7th/cmp-nvim-lua",
+    },
+    {
+        "L3MON4D3/LuaSnip",
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+    },
+    {
+        "williamboman/mason.nvim",
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+    },
+    {
+        "nvimtools/none-ls.nvim",
+    },
+    {
+        "georgewfraser/java-language-server",
+    },
+    {
+        "m4xshen/autoclose.nvim",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+    },
 })
